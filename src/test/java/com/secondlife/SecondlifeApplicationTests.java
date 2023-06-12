8ceaeabb59bd64ca2fe9792d0f6502c784495704
minsung <jiminsung@naver.com>
2023-06-12 09:20:29 +0900

    first commit

---
+++ src/test/java/com/secondlife/SecondlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
