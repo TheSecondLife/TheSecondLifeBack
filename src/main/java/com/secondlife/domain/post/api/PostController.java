37804db2f951c10e3d51cd751155774a98c35ded
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-06-15 15:28:01 +0900

    Post & Comment (Reply Not Yet)

--- src/main/java/com/secondlife/domain/post/api/PostController.java
+++ src/main/java/com/secondlife/domain/post/api/PostController.java
@@ -1,4 +1,62 @@
 package com.secondlife.domain.post.api;
 
+import com.secondlife.domain.post.dto.request.PostRegistRequestDto;
+import com.secondlife.domain.post.dto.request.PostUpdateRequestDto;
+import com.secondlife.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post/")
 public class PostController {
+
+    private final PostService postService;
+
+    // id로 Post 조회
+    @GetMapping("{id}")
+    public ResponseEntity<?> findPostById(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok()
+                .body(postService.findPostById(id));
+    }
+
+    // 전체 post 조회
+    @GetMapping("list")
+    public ResponseEntity<?> findPostList() {
+
+        return ResponseEntity.ok()
+                .body(postService.findPostList());
+    }
+
+    // post 등록
+    @PostMapping("regist/{userId}")
+    public ResponseEntity<?> registPost(@PathVariable("userId") Long userId, @RequestBody PostRegistRequestDto postRegistRequestDto) {
+
+        Long registId = postService.registPost(userId, postRegistRequestDto);
+
+        return ResponseEntity.ok()
+                .body(registId);
+    }
+
+    // post 수정
+    @PutMapping("update/{id}")
+    public ResponseEntity<?> updatePost(@PathVariable("id") Long id, @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
+
+        Long updateId = postService.updatePost(id, postUpdateRequestDto);
+
+        return ResponseEntity.ok()
+                .body(updateId);
+    }
+
+    // post 삭제
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable("id") Long id) {
+
+        Long deleteId = postService.deletePost(id);
+
+        return ResponseEntity.ok()
+                .body(deleteId);
+    }
 }
---
+++ src/test/java/com/secondlife/domain/post/service/PostServiceTest.java
@@ -0,0 +1,40 @@
+package com.secondlife.domain.post.service;
+
+import com.secondlife.domain.post.repository.PostRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void findPostById() {
+    }
+
+    @Test
+    void findPostList() {
+    }
+
+    @Test
+    void registPost() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+}
