47fa0a6fefcdb5ccb96a2de1adaceb087aadaedd
minsung <jiminsung@naver.com>
2023-06-18 02:20:51 +0900

    feat : crossOrigin 추가

--- src/main/java/com/secondlife/domain/user/service/KakaoLoginService.java
+++ src/main/java/com/secondlife/domain/user/service/KakaoLoginService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.secondlife.domain.user.dto.KakaoUserEnterDto;
 import com.secondlife.domain.user.dto.request.KakaoCodeRequestDto;
 import com.secondlife.domain.user.entity.User;
 import com.secondlife.domain.user.repository.UserRepository;
@@ -19,9 +20,11 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -35,28 +38,50 @@ public class KakaoLoginService {
     private static final String SUCCESS = "succes";
     private static final String FAIL = "fail";
 
-
+    @Transactional
     public ResponseEntity<?> kakaoLogin(KakaoCodeRequestDto requestDto){
         String access_token = getAccessToken(requestDto);
         HashMap<String, Object> userInfo = getUserInfo(access_token);
         HashMap<String, Object> JWT = getJWT(userInfo);
-        return new ResponseEntity<HashMap<String, Object>>(userInfo, HttpStatus.OK);
+        return new ResponseEntity<HashMap<String, Object>>(JWT, HttpStatus.OK);
     }
 
     public HashMap<String, Object> getJWT(HashMap<String, Object> userInfo) {
         if (isAvailable(userInfo.get("email").toString())) {
-            // 새로운 유저임 회원가입
-
+            enterUser(userInfo);
         }
         // 이메일로 유저 정보 조회후 JWT발행
+        Optional<User> checkUser = userRepository.findByEmail(userInfo.get("email").toString());
+        User user = checkUser.get();
         // JWT발행
-
-        return userInfo;
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        try {
+            result.put("access-token", jwtUtil.createToken(user.getEmail(), user.getPassword()));
+            result.put("message", SUCCESS);
+            result.put("email", user.getEmail());
+            result.put("name", user.getName());
+            result.put("id", user.getId());
+            result.put("age", user.getAge());
+            result.put("nickname", user.getNickname());
+            result.put("profileImg", user.getProfileImg());
+        } catch (UnsupportedEncodingException e) {
+            result.put("message", FAIL);
+        }
+        return result;
 
     }
 
-    public void enterUser() {
-
+    public void enterUser(HashMap<String, Object> userInfo) {
+        KakaoUserEnterDto enterUser = KakaoUserEnterDto
+                .builder()
+                .email(userInfo.get("email").toString())
+                .name(userInfo.get("nickname").toString())
+                .profileImg(userInfo.get("profileImg").toString())
+                .nickname(userInfo.get("nickname").toString())
+                .age(50)
+                .build();
+        User user = enterUser.toEntity();
+        userRepository.save(user);
     }
 
 
@@ -145,8 +170,11 @@ public class KakaoLoginService {
             nickname = element.getAsJsonObject().get("properties").getAsJsonObject().get("nickname").getAsString();
 
             boolean has_profileImg = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
-            if (has_profileImg)
+            if (has_profileImg) {
                 profileImg = element.getAsJsonObject().get("properties").getAsJsonObject().get("profile_image").getAsString();
+            } else {
+                profileImg = "https://fitsta-bucket.s3.ap-northeast-2.amazonaws.com/profile_default.jpg";
+            }
 
             userInfo.put("email", email);
             userInfo.put("nickname", nickname);
