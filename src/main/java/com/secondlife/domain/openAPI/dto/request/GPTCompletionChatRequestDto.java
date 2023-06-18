d37a39f07c98cf904e6f9b7d347f68c81ce6d56b
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-06-18 21:40:35 +0900

    ChatGPTAPI

---
+++ src/main/java/com/secondlife/domain/openAPI/dto/request/GPTCompletionChatRequestDto.java
@@ -0,0 +1,36 @@
+package com.secondlife.domain.openAPI.dto.request;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GPTCompletionChatRequestDto {
+
+    private String model;
+
+    private String role;
+
+    private String message;
+
+    private Integer maxTokens;
+
+    public static ChatCompletionRequest of(GPTCompletionChatRequestDto gptCompletionChatRequestDto) {
+
+        return ChatCompletionRequest.builder()
+                .model(gptCompletionChatRequestDto.getModel())
+                .messages(convertChatMessage(gptCompletionChatRequestDto))
+                .maxTokens(gptCompletionChatRequestDto.getMaxTokens())
+                .build();
+    }
+
+    private static List<ChatMessage> convertChatMessage(GPTCompletionChatRequestDto gptCompletionChatRequestDto) {
+
+        return List.of(new ChatMessage(gptCompletionChatRequestDto.getRole(), gptCompletionChatRequestDto.getMessage()));
+    }
+}
