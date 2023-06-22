8dbdaae44afedd69dbde7450dcd0928515fc141c
seyun <81186461+ParkSeYun98@users.noreply.github.com>
2023-06-22 18:00:04 +0900

    friend(진행중)

---
+++ src/main/java/com/secondlife/domain/friend/repository/FriendRepository.java
@@ -0,0 +1,31 @@
+package com.secondlife.domain.friend.repository;
+
+import com.secondlife.domain.friend.dto.response.FriendResponseDto;
+import com.secondlife.domain.friend.entity.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    @Query("select f.areWeFriend from Friend f " +
+            "where f.requestUser.id = :requestUserId and f.responseUser.id = :responseUserId")
+    Optional<Boolean> findFriendByRequestUserIdAndResponseUserId
+            (@Param("requestUserId") Long requestUserId, @Param("responseUserId") Long responseUserId);
+
+
+    @Query("select f from Friend f " +
+            "where f.requestUser.id = :requestUserId and f.responseUser.id = :responseUserId")
+    Optional<Friend> findFriendRequestByRequestUserIdAndResponseUserId
+            (@Param("requestUserId") Long requestUserId, @Param("responseUserId") Long responseUserId);
+
+
+
+    @Query("select new com.secondlife.domain.friend.dto.response.FriendResponseDto(f.responseUser)  " +
+            "from Friend f " +
+            "where f.areWeFriend = false and f.requestUser.id = :userId")
+    List<FriendResponseDto> findFriendListByUserId(@Param("userId") Long userId);
+}
