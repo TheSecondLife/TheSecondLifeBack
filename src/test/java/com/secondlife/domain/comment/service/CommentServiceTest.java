97a140ba4d30f2261fb7f67b152e7c80b8867df1
minsung <jiminsung@naver.com>
2023-06-27 22:24:04 +0900

    feat : 배포 자동화

---
+++ src/test/java/com/secondlife/domain/comment/service/CommentServiceTest.java
@@ -0,0 +1,44 @@
+//package com.secondlife.domain.comment.service;
+//
+//import com.secondlife.domain.comment.repository.CommentRepository;
+//import jakarta.persistence.EntityManager;
+//import jakarta.persistence.PersistenceContext;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//class CommentServiceTest {
+//
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    @Autowired
+//    private CommentRepository commentRepository;
+//
+//    @Test
+//    void findCommentById() {
+//    }
+//
+//    @Test
+//    void findCommentList() {
+//    }
+//
+//    @Test
+//    void findCommentListByPostId() {
+//    }
+//
+//    @Test
+//    void registComment() {
+//    }
+//
+//    @Test
+//    void updateComment() {
+//    }
+//
+//    @Test
+//    void deleteComment() {
+//    }
+//}
