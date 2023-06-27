50cb234af7e47822307c567d0772ab7cbb9d7887
minsung <jiminsung@naver.com>
2023-06-27 19:01:58 +0900

    feat : 배포 자동화

--- src/test/java/com/secondlife/SecondlifeApplicationTests.java
+++ src/test/java/com/secondlife/SecondlifeApplicationTests.java
@@ -3,7 +3,7 @@ package com.secondlife;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SecondlifeApplicationTests {
 
 	@Test
